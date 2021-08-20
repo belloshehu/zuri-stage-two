f98ad15c30de966c10ab6fac379383faec967f2c
Shehu Bello <belloshehu1@gmail.com>
2021-08-20 15:17:17 +0100

    created Resume component

---
+++ src/Resume.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './index.css';
+const Resume =()=>{
+    return(
+        <div id="container">
+            <h3>Application to join frontend developers team</h3>
+            <p>
+            Dear recruitment team,
+
+                I wish to apply for the position of Growth Hacker  in your reputable company.
+                Solving problems using programming languages and tools have been a life-time passion and dream.
+                Where I have worked previously, I had verse experience managing and interacting with customers.
+            </p>
+            <p>
+                I would like to bring the experience I have gained and build on it with your team of professionals.
+                Working among your highly  skilled professionals will enhance my skills and quench my thirst for learning new ways of solving problems. I believe that this job will help in bridging the gap between where I am now and where I  wish to be in my career journey.
+            
+            </p>
+            <p>
+                Thank you for going through my letter and I would be glad to have your invitation for interview. 
+                Kind regards.
+            </p>
+            <p>
+                
+            Shehu Bello 
+            </p>
+        </div>
+    )
+}
+export default Resume;
